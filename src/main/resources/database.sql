5c6ff829ad1b4a14e51204e967c0faa5b7c08c80
Artmal <artmalchik@gmail.com>
2017-04-24 07:11:45 +0300

    Update database.sql

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -24,18 +24,7 @@ CREATE TABLE user_roles (
   UNIQUE (user_id, role_id)
 )
   ENGINE = InnoDB;
-
--- Insert data
-
-INSERT INTO users VALUES (1, 'Artmal', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG');
-
-INSERT INTO roles VALUES (1, 'ROLE_USER');
-INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
-
-INSERT INTO user_roles VALUES (1, 2);
-
--- ------------------------------------------------------------------------
-
+  
 -- Table: playlists
 CREATE TABLE playlists(
   id INT AUTO_INCREMENT PRIMARY KEY,
